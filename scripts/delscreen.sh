032d317b29d01eee3a8b2ac9158aa57b76e5a78f
loungecube <cube@hannen.at>
2017-12-04 15:41:18 +0100

    more cosmetics

--- scripts/delscreen.sh
+++ scripts/delscreen.sh
@@ -6,8 +6,6 @@ xrandr --setprovideroutputsource nouveau Intel && display=$(xrandr | grep "mm x 
 
 if (($(grep -o "1" <<< "$display" | wc -l) > 1 ));
 then
-display=$(echo -e "$display" | dmenu -p "del:");
-xrandr --output $display --off;
-#echo -e "$display"
-#source ~/.xprofile;
+	display=$(echo -e "$display" | dmenu -p "del:");
+	xrandr --output $display --off;
 fi
