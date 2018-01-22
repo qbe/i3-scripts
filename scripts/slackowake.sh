a9432f9f32b2c20292c12ac805cfaf1c051e8515
Lukas Hannen <cube@hannen.at>
2018-01-22 14:37:54 +0100

    prevent wget logging
    redirected the logfile to /dev/null, leaving the "output document" as /dev/null

--- scripts/slackowake.sh
+++ scripts/slackowake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" \
---header="Accept-Encoding: compress, gzip" http://10.20.30.90:8080/slackomatic/rooms/lounge/powersaving/killswitch/reset --output-document=/dev/null
+--header="Accept-Encoding: compress, gzip" http://10.20.30.90:8080/slackomatic/rooms/lounge/powersaving/killswitch/reset --output-document=/dev/null --output-file=/dev/null
 
 #sends browserly-looking get-request to the slackomatic
 
