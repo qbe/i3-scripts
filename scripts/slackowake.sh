c0ed6711ce058ad63f95796c596cc93cd0393dd8
Lukas Hannen <cube@hannen.at>
2018-09-21 04:53:47 +0200

    optimize slackowake script
    Include immediate ssh command to send the first "killall midori" before actually starting bash

--- scripts/slackowake.sh
+++ scripts/slackowake.sh
@@ -9,7 +9,7 @@ wget --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTM
 #  exec "$HOME/.config/i3/midorikill.sh" > /dev/null
 #the expect line used to be its own script
 
-expect -c "spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@10.20.30.40; expect -re \".*assword: \"; send lounge\\r; expect -re \".*~\# \"; send \"killall midori\\r\"; send \"sleep 0.5\\r\"; send \"killall midori\\r\"; send \"sleep 0.5\\r\"; send \"killall midori\\r\"; send \"logout\\r\"; expect eof"
+expect -c "spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@10.20.30.40 \"killall midori; bash\" ; expect -re \".*assword: \"; send lounge\\r; expect -re \".*~\# \"; send \"killall midori\\r\"; send \"sleep 0.5\\r\"; send \"killall midori\\r\"; send \"sleep 0.5\\r\"; send \"killall midori\\r\"; send \"exit\\r\"; expect eof"
 #uses expect to send the browser kill command via ssh
 
 
