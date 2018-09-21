24e3f0e9b050dc2f7c50fee370eab4992e54711a
Lukas Hannen <cube@hannen.at>
2018-09-21 04:09:46 +0200

    fix "fail on host key change" bug
    make the midori killing lines ignore ssh host keys, effectively turning ssh into telnet

--- scripts/midorikill.sh
+++ scripts/midorikill.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn ssh root@10.20.30.40 killall midori
+spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@10.20.30.40 killall midori
 expect "assword:"
 send "lounge\r"
 interact
