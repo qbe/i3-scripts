bb780ac786673e2d4f24a95af9e4d4988b2816e0
Lukas Hannen <cube@hannen.at>
2018-09-23 22:33:58 +0200

    re-fixed addscreen and delscreen files
    finally did the variable quoting right

--- scripts/addscreen.sh
+++ scripts/addscreen.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-#default display to ADD is VGA1. if everything fails, it tries to add LVDS1
+#default display to ADD is LVDS1. if everything fails, it tries to add LVDS1
 display="LVDS1"
 
 #display is set to a list of connected displays which are not used
@@ -8,16 +7,16 @@ xrandr --setprovideroutputsource nouveau Intel && display="$(xrandr | grep ' con
 
 #if the list of displays that are connected but not in use is more than one element long,
 #launch a dmenu bar to select which entry to keep in the list
-if [ "$(grep -o '1' <<< \"$display\" | wc -l)" -gt 1 ];
+if [ "$(grep -o '1' <<< "$display" | wc -l)" -gt 1 ];
 then
-	display="$(echo -e \"$display\" | dmenu -p \"add:\")";
+	display="$(echo -e "$display" | dmenu -p add:)"
 fi
 
 #If the display to add is not LVDS1, add it in relation to LVDS1 according to the first argument
 #If the display to add is LVDS1, ask via dmenu in relation to which display the adding shall occur
 if [ "$display" == "LVDS1" ];
 then
-	xrandr --output "$display" --auto "--$1" "$(xrandr | grep 'mm x ' | awk '{print $1}' | dmenu -p \"$1:\")"
+	xrandr --output "$display" --auto "--$1" "$(xrandr | grep 'mm x ' | awk '{print $1}' | dmenu -p "$1":)"
 else
 	xrandr --output "$display" --auto "--$1" LVDS1
 fi
--- scripts/delscreen.sh
+++ scripts/delscreen.sh
@@ -9,7 +9,7 @@ xrandr --setprovideroutputsource nouveau Intel && display="$(xrandr | grep 'mm x
 
 if (($(grep -o "1" <<< "$display" | wc -l) > 1 ));
 then
-	display=$(echo -e "$display" | dmenu -p "del:");
+	display="$(echo -e "$display" | dmenu -p del: )"
 	xrandr --output "$display" --off;
 fi
 
